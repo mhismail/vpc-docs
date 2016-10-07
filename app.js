f437301a88f3434092a1814996e62452b57d88d0
ronkeizer <ronkeizer@gmail.com>
2016-10-07 09:18:19 -0700

    different folder name

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/_book'));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 8080);

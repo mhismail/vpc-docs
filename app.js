766e330c7bc3b0aa92d0307ed02b5dfdc50fa9d3
ronkeizer <ronkeizer@gmail.com>
2016-10-07 09:02:59 -0700

    add static server

---
+++ app.js
@@ -0,0 +1,12 @@
+var static = require('node-static');
+
+var file = new static.Server('./_book');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+    }).resume();
+}).listen(process.env.PORT || 8080);
